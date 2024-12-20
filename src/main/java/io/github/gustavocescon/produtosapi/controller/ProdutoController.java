f1ac618062e49165cc0e77951259c5d232d15651
Gustavo Estanislau Cescon de Oliveira <gustavo.cescon@prefeitura.local>
2024-12-20 10:56:27 -0300

    initial commit

---
+++ src/main/java/io/github/gustavocescon/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,53 @@
+package io.github.gustavocescon.produtosapi.controller;
+
+import io.github.gustavocescon.produtosapi.model.Produto;
+import io.github.gustavocescon.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto) {
+        System.out.println("procudo recebido: " + produto);
+        produto.setId(UUID.randomUUID().toString());
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        //Optional<Produto> produto = produtoRepository.findById(id);
+        //return produto.isPresent() ? produto.get() : null;
+
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizar(@PathVariable("id") String id,
+                          @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping()
+    public List<Produto> search(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
+    }
+
+}
